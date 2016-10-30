8738e56897d17ce5b3b49fe1da217a2a8883e09d
Alexander Kurbanov <kurbanov.alex@gmail.com>
2016-10-30 23:23:58 +0300

    Add SDL

--- emu-space-invaders/main.cpp
+++ emu-space-invaders/main.cpp
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "processor.h"
 #include <Windows.h>
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 /*
 Links: 
@@ -28,7 +34,31 @@ uint16 ReadFileIntoMemory(uint8 *memory, char *filename, uint16 offset) {
     return fileSize;
 }
 
-int main() {
+int main(int argc, char *args[]) {
+	SDL_Window *window = NULL;
+	SDL_Surface *screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	} else {
+		window = SDL_CreateWindow("SDL Tutorial",
+			SDL_WINDOWPOS_UNDEFINED, 
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH, 
+			SCREEN_HEIGHT, 
+			SDL_WINDOW_SHOWN);
+		if (window == NULL) {
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		} else {
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+		}
+	}
+
     State8080 state = {};
     state.memory = (uint8 *)malloc(0x10000); // 65KB
 
